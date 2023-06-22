7c323161515c9356be878cb0f5ca2a0ee326b70c
chandi23598 <137384072+chandi23598@users.noreply.github.com>
2023-06-22 13:32:12 +0530

    Add files via upload

---
+++ digitalClock.js
@@ -0,0 +1,105 @@
+function digitclk()
+{
+    var date=new Date();
+    var hh=date.getHours();
+    var mm=date.getMinutes();
+    var ss=date.getSeconds();
+    // var ml=date.getMilliseconds();
+    var dt=date.getDate();
+    var mo=date.getMonth();
+    var yy=date.getFullYear();
+    var day=date.getDay();
+    console.log(hh,mm,ss,dt,mo,yy,day)
+
+    var am_pm="AM"
+    if(hh>=12)
+    {
+        am_pm="PM"
+        if(hh>12)
+        {
+            hh-=12;
+        }
+    }
+    if(hh==0)
+    {
+        hh=12;
+    }
+    if(mo==0)
+    {
+        mo="Jan"
+    }
+    else if(mo==1)
+    {
+        mo="Feb"
+    }
+    else if(mo==2)
+    {
+        mo="Mar"
+    }
+    else if(mo==3)
+    {
+        mo="Apr"
+    }
+    else if(mo==4)
+    {
+        mo="May"
+    }
+    else if(mo==5)
+    {
+        mo="Jun"
+    }
+    else if(mo==6)
+    {
+        mo="Jul"
+    }
+    else if(mo==7)
+    {
+        mo="Aug"
+    }
+    else if(mo==8)
+    {
+        mo="Sep"
+    }
+    else if(mo==9)
+    {
+        mo="oct"
+    }
+    else if(mo==10)
+    {
+        mo="Nov"
+    }
+    else
+    {
+        mo="Dec"
+    }
+    switch(day)
+    {
+        case 0:day="Sunday"
+        document.body.style.backgroundImage="url(../Gallery/photos/0.jpg)"
+        break;
+        case 1:day="Monday"
+        document.body.style.backgroundImage="url(../Gallery/photos/1.jpg)"
+        break;
+        case 2:day="Tuesday"
+        document.body.style.backgroundImage="url(../Gallery/photos/2.jpg)"
+        break;
+        case 3:day="Wednesday"
+        document.body.style.backgroundImage="url(../Gallery/photos/3.jpg)"
+        break;
+        case 4:day="Thursday"
+        document.body.style.backgroundImage="url(../Gallery/photos/4.jpg)"
+        break;
+        case 5:day="Friday"
+        document.body.style.backgroundImage="url(../Gallery/photos/5.jpg)"
+        break;
+        case 6:day="Saturday"
+        document.body.style.backgroundImage="url(../Gallery/photos/6.jpg)"
+        break;        
+    }
+    document.getElementById("Date").innerHTML=`${dt}/${mo}/${yy}`;
+    document.getElementById("Time").innerHTML=`${hh}:${mm} ${am_pm}`;
+    document.getElementById("Day").innerHTML=day;
+    document.getElementById("sec").innerHTML=ss;
+    setTimeout(digitclk,1000)
+}
+digitclk()
